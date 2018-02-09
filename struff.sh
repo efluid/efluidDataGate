520d9409e6b60824af5a87c41bb27b71189ad2c2
Emmanuel Lecomte <emmanuel.lecomte@zenika.com>
2018-02-09 17:44:12 +0100

    Revised build

--- struff.sh
+++ struff.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 ## As a stuff pipeline, the context is :
-## Current user has limited rights
-## Current user can use docker
-## Current working directory is root of checkout sources
-## Docker compose is used in instance
-## The instance location is "./.instance"
+## - Current user has limited rights
+## - Current user can use docker
+## - Current working directory is root of checkout sources
+## - To use ref to local directory on volumn mount for docker, you can use $1
+## - Docker compose is used in instance
+## - The instance location is "./.instance"
 
 ## MVN build
 docker run -it --rm \
     -v /opt/server/build:/root/.m2 \
-    -v ./:/usr/src/mymaven \
+    -v $1:/usr/src/mymaven \
     -w /usr/src/mymaven maven:3.3-jdk-8 /bin/bash \
     -c "mvn --batch-mode install -D skipTests; cp /usr/src/mymaven/src/docker/* /usr/src/mymaven/target"
 
