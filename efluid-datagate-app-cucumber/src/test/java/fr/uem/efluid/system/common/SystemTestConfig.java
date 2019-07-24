107d0252e875680d90570fcfc834f0ce18e15c64
Emmanuel Lecomte <emmanuel.lecomte@zenika.com>
2019-07-24 11:34:06 +0200

    :construction: Fixed items for testing after merge

--- efluid-datagate-app-cucumber/src/test/java/fr/uem/efluid/system/common/SystemTestConfig.java
+++ efluid-datagate-app-cucumber/src/test/java/fr/uem/efluid/system/common/SystemTestConfig.java
@@ -1,13 +1,10 @@
 package fr.uem.efluid.system.common;
 
-import static fr.uem.efluid.Application.Packages.*;
-import static fr.uem.efluid.system.common.SystemTestConfig.SYS_TEST_ROOT;
-
-import java.sql.SQLException;
-
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
-
+import fr.uem.efluid.system.stubs.TweakedAsyncDriver;
+import fr.uem.efluid.tools.AsyncDriver;
+import fr.uem.efluid.tools.ManagedQueriesGenerator.QueryGenerationRules;
+import fr.uem.efluid.utils.DatasourceUtils;
+import fr.uem.efluid.utils.DatasourceUtils.CustomDataSourceParameters;
 import org.h2.server.web.WebServer;
 import org.h2.tools.Server;
 import org.slf4j.Logger;
@@ -26,104 +23,105 @@ import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 
-import fr.uem.efluid.system.stubs.TweakedAsyncDriver;
-import fr.uem.efluid.tools.AsyncDriver;
-import fr.uem.efluid.tools.ManagedQueriesGenerator.QueryGenerationRules;
-import fr.uem.efluid.utils.DatasourceUtils;
-import fr.uem.efluid.utils.DatasourceUtils.CustomDataSourceParameters;
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+import static fr.uem.efluid.Application.Packages.*;
+import static fr.uem.efluid.system.common.SystemTestConfig.SYS_TEST_ROOT;
 
 /**
  * <p>
  * Default config for fully contextualised tests
  * </p>
- * 
+ *
  * @author elecomte
- * @since v0.0.1
  * @version 1
+ * @since v0.0.1
  */
-@EnableJpaRepositories({ REPOSITORIES, SYS_TEST_ROOT + ".stubs.repositories" })
-@EntityScan({ ENTITIES, CONVERTERS, SYS_TEST_ROOT + ".stubs.entities" })
-@ComponentScan({ CONFIG, SERVICES, REPOSITORIES_IMPLS, TOOLS, WEB, REST, SYS_TEST_ROOT + ".stubs" })
+@EnableJpaRepositories({REPOSITORIES, SYS_TEST_ROOT + ".stubs.repositories"})
+@EntityScan({ENTITIES, CONVERTERS, SYS_TEST_ROOT + ".stubs.entities"})
+@ComponentScan({CONFIG, SERVICES, REPOSITORIES_IMPLS, TOOLS, WEB, REST, SYS_TEST_ROOT + ".stubs"})
 @TestConfiguration
 @ConfigurationProperties(prefix = "datagate-efluid.managed-datasource")
 public class SystemTestConfig extends CustomDataSourceParameters {
 
-	public static final String SYS_TEST_ROOT = ROOT + ".system";
+    public static final String SYS_TEST_ROOT = ROOT + ".system";
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(SystemTestConfig.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(SystemTestConfig.class);
 
-	@Autowired
-	private DataSource defaultDataSource;
+    @Autowired
+    private DataSource defaultDataSource;
 
-	@Autowired
-	private EntityManagerFactory defaultEntityManagerFactory;
+    @Autowired
+    private EntityManagerFactory defaultEntityManagerFactory;
 
-	@Bean
-	public JdbcTemplate managedDatabaseJdbcTemplate() {
-		return new JdbcTemplate(this.defaultDataSource);
-	}
+    @Bean
+    public JdbcTemplate managedDatabaseJdbcTemplate() {
+        return new JdbcTemplate(this.defaultDataSource);
+    }
 
-	/**
-	 * @return
-	 * @throws SQLException
-	 */
-	@Bean(initMethod = "start", destroyMethod = "stop")
-	public Server h2WebConsole() throws SQLException {
-		LOGGER.info("H2 CONSOLE activated on http://localhost:8082");
-		return new Server(new WebServer(), "-web", "-webAllowOthers", "-webPort", "8082");
-	}
+    /**
+     * @return
+     * @throws SQLException
+     */
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server h2WebConsole() throws SQLException {
+        LOGGER.info("H2 CONSOLE activated on http://localhost:8082");
+        return new Server(new WebServer(), "-web", "-webAllowOthers", "-webPort", "8082");
+    }
 
-	/**
-	 * @return
-	 */
-	@Bean(name = DatasourceUtils.MANAGED_TRANSACTION_MANAGER)
-	public PlatformTransactionManager managedTransactionManager() {
-		return new DataSourceTransactionManager(this.defaultDataSource);
-	}
+    /**
+     * @return
+     */
+    @Bean(name = DatasourceUtils.MANAGED_TRANSACTION_MANAGER)
+    public PlatformTransactionManager managedTransactionManager() {
+        return new DataSourceTransactionManager(this.defaultDataSource);
+    }
 
-	/**
-	 * @return
-	 */
-	@Bean(name = DatasourceUtils.DEFAULT_TRANSACTION_MANAGER)
-	@Primary
-	public PlatformTransactionManager defaultTransactionManager() {
-		return new JpaTransactionManager(this.defaultEntityManagerFactory);
-	}
+    /**
+     * @return
+     */
+    @Bean(name = DatasourceUtils.DEFAULT_TRANSACTION_MANAGER)
+    @Primary
+    public PlatformTransactionManager defaultTransactionManager() {
+        return new JpaTransactionManager(this.defaultEntityManagerFactory);
+    }
 
-	@Bean
-	public QueryGenerationRules managedQueryGenerationRules() {
-		// Use local query config directly
-		QueryGenerationRules rules = this.getQuery();
-		LOGGER.info("[MANAGED DB] Using these query generation rules : columnProtected:{}, tableProtected:{}",
-				Boolean.valueOf(rules.isColumnNamesProtected()), Boolean.valueOf(rules.isTableNamesProtected()));
-		return rules;
-	}
+    @Bean
+    public QueryGenerationRules managedQueryGenerationRules() {
+        // Use local query config directly
+        QueryGenerationRules rules = this.getQuery();
+        LOGGER.info("[MANAGED DB] Using these query generation rules : columnProtected:{}, tableProtected:{}",
+                Boolean.valueOf(rules.isColumnNamesProtected()), Boolean.valueOf(rules.isTableNamesProtected()));
+        return rules;
+    }
 
-	/**
-	 * @return
-	 */
-	@Bean
-	public FilterRegistrationBean<InspectSessionFilter> inspectSessionFilter() {
+    /**
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean<InspectSessionFilter> inspectSessionFilter() {
 
-		LOGGER.info("[INSPECT] Add session filter");
+        LOGGER.info("[INSPECT] Add session filter");
 
-		FilterRegistrationBean<InspectSessionFilter> registration = new FilterRegistrationBean<>();
-		registration.setFilter(new InspectSessionFilter());
-		registration.setName("inspectSessionFilter");
-		registration.setOrder(2);
+        FilterRegistrationBean<InspectSessionFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new InspectSessionFilter());
+        registration.setName("inspectSessionFilter");
+        registration.setOrder(2);
 
-		return registration;
-	}
+        return registration;
+    }
 
-	/**
-	 * <p>
-	 * For system tests, use a specific "programable" async driver
-	 * </p>
-	 * 
-	 * @return
-	 */
-	@Bean
-	public AsyncDriver futureAsyncDriver() {
-		return new TweakedAsyncDriver();
-	}
+    /**
+     * <p>
+     * For system tests, use a specific "programable" async driver
+     * </p>
+     *
+     * @return
+     */
+    @Bean
+    public AsyncDriver futureAsyncDriver() {
+        return new TweakedAsyncDriver();
+    }
 }
