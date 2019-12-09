346fd09fdcddff0d4481a7f78c7240ad1f5ccde3
Marie Coste <m-coste-externe@efluid.fr>
2019-12-09 16:05:15 +0100

    [fix] update dockerFile for jdk11

--- efluid-datagate-app/src/docker/build-serv-efluid/standalone-with-h2/build.sh
+++ efluid-datagate-app/src/docker/build-serv-efluid/standalone-with-h2/build.sh
@@ -7,7 +7,7 @@ docker run -it --rm \
     -v /opt/apache-maven-3.3.9/conf/settings.xml:/root/.m2/settings.xml \
     -v /data/EDT/mavenRepository:/data/EDT/mavenRepository \
     -v $(pwd):/usr/src/mymaven \
-    -w /usr/src/mymaven maven:3.5-jdk-10 /bin/bash \
+    -w /usr/src/mymaven maven:3.5-jdk-11 /bin/bash \
     -c "mvn --global-settings /root/.m2/settings.xml --batch-mode -pl '!efluid-datagate-app-tests' install -DskipTests; cp /usr/src/mymaven/efluid-datagate-app/src/docker/build-serv-efluid/standalone-with-h2/* /usr/src/mymaven/efluid-datagate-app/target; cp /usr/src/mymaven/efluid-datagate-app/src/docker/build-serv-efluid/logback.xml /usr/src/mymaven/efluid-datagate-app/target"
 
 ## Docker build
