9cd3a111c0997b45c863c6e1c7c08b2776503808
Emmanuel LECOMTE <emmanuel.lecomte@zenika.com>
2020-04-10 11:28:07 +0200

    :construction: Init test case for HR payload build

--- efluid-datagate-app/src/test/java/fr/uem/efluid/tools/ManagedValueConverterUnitTest.java
+++ efluid-datagate-app/src/test/java/fr/uem/efluid/tools/ManagedValueConverterUnitTest.java
@@ -36,4 +36,15 @@ public class ManagedValueConverterUnitTest {
 		Assert.assertEquals("Other", values.get("PRESET").getValueAsString());
 		Assert.assertEquals("123400", values.get("SOMETHING").getValueAsString());
 	}
+
+	@Test
+	public void testConvertToHrPayloadCreateWithLob(){
+
+		String testCreatePayload = "ACTIF=O/MQ==,BINARY_VALUE=B/VFhHwmXCOK8+2N2G35sYpTsc6ltH++IFu9OjALrntiA=,CREATE_DATE=T/MjAxOC0wMS0wMiAyMzo0MjozNA==,DESCRIPTION=S/VW4gbW9kw6hsZSBkZSB0ZXN0IC0gMw==,FABRICANT=S/RFVQT04=,TYPEID=O/Mw==";
+
+		// String hrPayload = this.converter.convertToHrPayload(?,?);
+
+		// Case : We check that for a specified payload corresponding to a value create, the provided result of HR Payload is exactly what's expected
+
+	}
 }
