36832076607d0aceb1d8ab7f3c25a3dde2cca5c3
Emmanuel LECOMTE <emmanuel.lecomte@zenika.com>
2020-06-21 18:05:48 +0200

    :construction: Test new log config init in docker

--- efluid-datagate-app/src/docker/build-ci/run.sh
+++ efluid-datagate-app/src/docker/build-ci/run.sh
@@ -4,13 +4,25 @@
 
 ## CONFIG FILES
 app_cfg="/cfg/application.yml"
-log_cfg="/cfg/logback.xml"
 
 ## OPTIONS FOR DEBUG
 activate_debug_param="-agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=y"
-java_option="-Xmx$xmx -Xms$xms -jar /app/efluid-datagate-app-exec.jar --spring.config.location=classpath:/application.yml,file:$app_cfg --logging.config=file:$log_cfg"
+activate_log_param="--logging.file=$customLogging"
+java_option="-Xmx$xmx -Xms$xms -jar /app/efluid-datagate-app-exec.jar --spring.config.location=/application.yml,file:$app_cfg"
 
 echo "STARTING EFLUID-DATAGATE"
+
+## IF SPECIFIED, ENABLE CUSTOM LOGGING
+if [ "$customLogging" != "disabled" ]; then
+    java_option="$java_option $activate_log_param"
+    echo "CUSTOM LOGGING ENABLED"
+fi
+
+## IF SPECIFIED, ENABLE DEBUG
+if [ "$remoteXdebug" = "true" ]; then
+    java_option="$activate_debug_param $java_option"
+    echo "DEBUG MODE ENABLED"
+fi
 
 ## LAUNCH APP
 $JAVA_HOME/bin/java $java_option
