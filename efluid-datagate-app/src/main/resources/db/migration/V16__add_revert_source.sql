93a76b3abaa8abe073073f8bb352f3d9f5e66717
Emmanuel LECOMTE <emmanuel.lecomte@zenika.com>
2021-03-12 15:33:12 +0100

    :construction: Fix revert commit display

--- efluid-datagate-app/src/main/resources/db/migration/V16__add_revert_source.sql
+++ efluid-datagate-app/src/main/resources/db/migration/V16__add_revert_source.sql
@@ -1,2 +1,8 @@
 -- ADD COLUMN REVERT_SOURCE_UUID IN COMMITS TABLE FOR REVERT LINK
-ALTER TABLE "COMMITS" ADD "REVERT_SOURCE_UUID" VARCHAR2(255);
+declare
+    column_exists exception;
+    pragma exception_init (column_exists , -01430);
+begin
+    execute immediate 'ALTER TABLE "COMMITS" ADD "REVERT_SOURCE_UUID" VARCHAR2(255)';
+exception when column_exists then null;
+end;
